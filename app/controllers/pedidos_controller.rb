89940b36b1179ede821ab1549c3e2c4ea7b288ec
Élisson Michael <elissonmichael@yahoo.com.br>
2018-11-01 15:16:37 -0300

    Adiciona Pedidos do Dia

--- app/controllers/pedidos_controller.rb
+++ app/controllers/pedidos_controller.rb
@@ -1,45 +1,57 @@
 class PedidosController < ApplicationController
+  before_action :set_pedido, only: %i[edit update destroy entregar]
+  before_action :set_dia, only: %i[index new]
+
   def index
-    @pedidos = Pedido.all
+    @pedidos = Pedido.where(data: @dia)
   end
 
   def new
-    @pedido = Pedido.new(data: Date.today, valor: 0.0)
+    @pedido = Pedido.new(data: @dia, valor: 0.0)
   end
 
   def create
     @pedido = Pedido.new(pedido_params)
     if @pedido.save
-      redirect_to pedidos_path
+      redirect_to pedidos_path(dia: @pedido.data)
     else
       render :new
     end
   end
 
-  def edit
-    @pedido = Pedido.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @pedido = Pedido.find(params[:id])
     if @pedido.update(pedido_params)
-      redirect_to pedidos_path
+      redirect_to pedidos_path(dia: @pedido.data)
     else
       render :edit
     end
   end
 
   def entregar
-    Pedido.find(params[:id]).entregar
-    redirect_to pedidos_path
+    @pedido.entregar
+    redirect_to pedidos_path(dia: @pedido.data)
   end
 
   def destroy
-    Pedido.find(params[:id]).destroy
-    redirect_to pedidos_path
+    @pedido.destroy
+    redirect_to pedidos_path(dia: @pedido.data)
   end
 
   private
+
+  def set_pedido
+    @pedido = Pedido.find(params[:id])
+  end
+
+  def set_dia
+    @dia =  if params[:dia].present?
+              params[:dia].to_date
+            else
+              Date.today
+            end
+  end
 
   def pedido_params
     params.require(:pedido).permit(:descricao, :valor, :data)
