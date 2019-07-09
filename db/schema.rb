4610d05f8188ebdd0590a2b0b145f711a5bf0cba
Élisson Michael <elissonmichael@yahoo.com.br>
2019-07-08 21:05:14 -0300

    Update DB Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190825142530) do
+ActiveRecord::Schema.define(version: 20190825144810) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
     t.integer "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "media_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tracks", force: :cascade do |t|
+    t.string "name"
+    t.integer "album_id"
+    t.integer "media_type_id"
+    t.integer "genre_id"
+    t.string "composer"
+    t.integer "milliseconds"
+    t.integer "bytes"
+    t.float "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
