9c996212d5f1fe5aff6e49f150a6f9a59d459708
Élisson Michael <elissonmichael@yahoo.com.br>
2019-07-10 21:05:14 -0300

    Fix Validations Problems While Seeding

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,6 +3,6 @@ class Track < ApplicationRecord
   belongs_to :genre
   belongs_to :media_type
 
-  validates_presence_of :name, :album, :media_type, :genre, :composer,
+  validates_presence_of :name, :album, :media_type, :genre,
                         :milliseconds, :bytes, :unit_price
 end
