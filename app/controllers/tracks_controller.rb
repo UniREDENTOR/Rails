fb5f297a0ad819fa147470eda16503db7897484e
Bruno <bruno_151997@hotmail.com>
2019-10-19 17:37:00 -0300

    Ref #3 Adiciona Edit/update Controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -16,19 +16,20 @@ class TracksController < ApplicationController
       render :new
     end
   end
-  private
-  def track_params
-    params.require(:track).permit(:name, :album_id, :media_type_id, :genre_id,:composer, :milliseconds, :bytes, :unit_price)
-  end
-  def edit
-    @track = Track.find(params[:id])
-  end
-  def update
+    def edit
       @track = Track.find(params[:id])
+     end
+  def update
+       @track = Track.find(params[:id])
     if @track.update_attributes(track_params)
        redirect_to @track
     else
        render :edit
     end
   end
+  private
+  def track_params
+    params.require(:track).permit(:name, :album_id, :media_type_id, :genre_id,:composer, :milliseconds, :bytes, :unit_price)
+  end
 end
+  
