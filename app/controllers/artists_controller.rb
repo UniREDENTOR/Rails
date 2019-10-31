bb42d0369b6a9d0ffbc12cc651340692e35695d6
Rodolpho Lopes <dolpholopes@gmail.com>
2019-10-31 04:21:54 +0000

    Controller do artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,32 @@
 class ArtistsController < ApplicationController
   def index
-      @consulta = Artist.all
+      @artist = Artist.all
+      if params['name'].present?
+      @artist = @artist.where("name LIKE ?", "%#{params['name']}%")
+    end
   end
+
+  def show
+    @artist = Artist.find(params[:id]).albums
+  end
+
+  def new
+    @artist = Artist.new
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to @artist
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:name)
+  end
+
 end
