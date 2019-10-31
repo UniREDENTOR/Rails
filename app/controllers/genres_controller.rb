4a570430273975a9330cf839f4f2afdc86f6e806
Rodolpho Lopes <dolpholopes@gmail.com>
2019-10-31 04:22:13 +0000

    Controller de genres

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,6 +1,9 @@
 class GenresController < ApplicationController
   def index
-    @genres = Genre.filtrar(params['name'])
+     @genre = Genre.all
+     if params['name'].present?
+      @genre = @genre.where("name LIKE ?", "%#{params['name']}%")
+    end
   end
 
   def show
@@ -25,4 +28,36 @@ class GenresController < ApplicationController
   def genre_params
     params.require(:genre).permit(:name)
   end
+
+endclass GenresController < ApplicationController
+  def index
+     @genre = Genre.all
+     if params['name'].present?
+      @genre = @genre.where("name LIKE ?", "%#{params['name']}%")
+    end
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      redirect_to @genre
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
+  end
+
 end
