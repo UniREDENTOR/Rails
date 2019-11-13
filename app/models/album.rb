a74de536826dc43e682fb84763bbb68b26cdda8d
Élisson Michael <elissonmichael@gmail.com>
2019-11-13 19:33:01 -0300

    Adiciona Dependência nas Associações de Album e Artista

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ApplicationRecord
   belongs_to :artist
-  has_many :tracks
+  has_many :tracks, dependent: :destroy
 
   validates_presence_of :title, :artist
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :albums
+  has_many :albums, dependent: :destroy
 
   validates_presence_of :name
 end
