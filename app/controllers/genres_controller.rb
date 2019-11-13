19afae66666285dcdad0c634d5532a35841a61a5
Élisson Michael <elissonmichael@gmail.com>
2019-11-13 18:55:53 -0300

    Adiciona Edição de Gênero

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -20,6 +20,19 @@ class GenresController < ApplicationController
     end
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to @genre
+    else
+      render :edit
+    end
+  end
+
   private
 
   def genre_params
